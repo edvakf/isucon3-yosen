2caa5b7e06d2749baa67257ef5dd26e980abba0e
isucon <isucon@isucon.net>
2013-10-05 16:08:26 +0900

    increment total cache instead of delete

--- index.php
+++ index.php
@@ -319,7 +319,7 @@ dispatch_post('/memo', function() {
     $stmt->execute();
 
     $memo_id = $db->lastInsertId();
-    $total = apc_delete('memo_private_total');
+    $total = apc_inc('memo_private_total');
     return redirect('/memo/' . $memo_id);
 });
 
