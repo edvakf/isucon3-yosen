2859aab9f3de94cc838bb8c24174baa73f081fef
isucon <isucon@isucon.net>
2013-10-05 15:29:38 +0900

    Session_APC

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
 //ini_set( 'session.save_path', 'localhost:11211' );
 
 require_once 'limonade/lib/limonade.php';
+require_once 'session_apc.php';
 
 function configure()
 {
@@ -375,7 +376,7 @@ dispatch_get('/memo/:id', function() {
     return html('memo.html.php');
 });
 
-
+new Session_APC;
 run();
 
 
